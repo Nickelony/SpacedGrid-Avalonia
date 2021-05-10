f7ee74a8693e6a2e04073a0e91a7b1af93791347
Nickelony <nickelkwn@gmail.com>
2021-05-10 12:50:09 +0200

    Updated demo.

--- SpacedGrid.Avalonia.Demo/MainWindow.axaml.cs
+++ SpacedGrid.Avalonia.Demo/MainWindow.axaml.cs
@@ -1,21 +1,48 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using System;
 
 namespace SpacedGrid.Avalonia.Demo
 {
 	public partial class MainWindow : Window
 	{
+		private SpacedGrid spacedGrid;
+		private TextBlock textBlock_RowSpacing;
+		private TextBlock textBlock_ColumnSpacing;
+
 		public MainWindow()
 		{
 			InitializeComponent();
 #if DEBUG
             this.AttachDevTools();
 #endif
+			spacedGrid = this.FindControl<SpacedGrid>("SpacedGrid");
+			textBlock_RowSpacing = this.FindControl<TextBlock>("textBlock_RowSpacing");
+			textBlock_ColumnSpacing = this.FindControl<TextBlock>("textBlock_ColumnSpacing");
 		}
 
 		private void InitializeComponent()
 		{
 			AvaloniaXamlLoader.Load(this);
+		}
+
+		private void RowSpacingSliderPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+		{
+			if (e.Property.Name.Equals("Value", StringComparison.OrdinalIgnoreCase))
+			{
+				spacedGrid.RowSpacing = (double)e.NewValue!;
+				textBlock_RowSpacing.Text = e.NewValue.ToString();
+			}
+		}
+
+		private void ColumnSpacingSliderPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+		{
+			if (e.Property.Name.Equals("Value", StringComparison.OrdinalIgnoreCase))
+			{
+				spacedGrid.ColumnSpacing = (double)e.NewValue!;
+				textBlock_ColumnSpacing.Text = e.NewValue.ToString();
+			}
 		}
 	}
 }
